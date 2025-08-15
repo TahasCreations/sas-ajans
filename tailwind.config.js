e677c811bc200d57c7cb7733b26faf32ef8cd8d5
Taha <bentahasarii@gmail.com>
2025-08-15 20:05:04 +0300

    İkinci commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'pacifico': ['var(--font-pacifico)', 'cursive'],
-      },
       colors: {
         primary: {
           50: '#fefce8',
@@ -27,7 +24,6 @@ module.exports = {
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in-out',
         'slide-up': 'slideUp 0.5s ease-out',
-        'bounce-slow': 'bounce 2s infinite',
       },
       keyframes: {
         fadeIn: {
